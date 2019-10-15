f838a8c5e4e7cef0b12547b86449b15380e77633
ElzaCS <49568678+ElzaCS@users.noreply.github.com>
2019-10-15 18:26:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProjCards from './components/ProjCards/ProjCards';
+import MentorList from './components/MentorList/MentorList';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={LandingPage}></Route>
             <Route exact path="/org/:id/:orgName" component={ProjCards}></Route>
+            <Route exact path="/mentorlist" component={MentorList}></Route>
           </Switch>
         </Router>
       </div>
